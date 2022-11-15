e5b98701e2aaf7416cabd96ea5d52e16b80fb754
jonesmj14 <jonesmj14@gmail.com>
2022-11-14 22:23:20 -0800

    adding netkiko first pyhon2on

---
+++ testprompt.py
@@ -0,0 +1,12 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+net_connect = ConnectHandler(
+    host='cisco3.lasthop.io',
+    username='pyclass',
+    password=getpass(),
+    device_type='cisco_ios',
+    session_log='my_session.txt',
+)
+
+print (net_connect.find_prompt())
